423e4ba05b709c93de9b7f92e807b70bd524868b
Ankush1722 <88156996+Ankush1722@users.noreply.github.com>
2023-08-17 23:00:33 +0530

    Delete AI directory

--- AI/ASS_1/ques7.py
+++
@@ -1,7 +0,0 @@
-import ques7func
-principal=int(input("Enter the principal amt.:"))
-rate=float(input("Enter the rate of interest:"))
-time=float(input("Enter time in years:"))
-n=int(input("Enter number of times interest applied per years:"))
-C=ques7func.fn_CI(principal,rate,time,n)
-print(C)
--- AI/ASS_1/ques7func.py
+++
@@ -1,3 +0,0 @@
-def fn_CI(principal,rate,time,n) :
-    C=principal*((1+(rate/n))**time)
-    return C
